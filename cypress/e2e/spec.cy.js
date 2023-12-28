04326acbe50bff6b05d2cccc14a26d471c4e6a8e
Vitor Ponce <vitor.ponce20@gmail.com>
2023-12-28 16:22:52 -0300

    [2] include page open

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -4,3 +4,23 @@ describe('template spec', () => {
       
   })
 })
+
+context('my first test', ()=>{
+  beforeEach(() => {
+    cy.visit('https://example.cypress.io')
+  })
+
+  it('has an h1 on the page', () => {
+    cy.get('h1').should('exist')
+
+  })
+
+  it('access querying page', () => {
+    cy.get('li a').contains('Querying').click({})
+    cy.get('h1').should('exist')
+    
+    
+  })
+
+} )
+
